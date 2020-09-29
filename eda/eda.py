d41f04274a263f99de4c5c9609efb818a742be52
ghPRao <prabhakar.rangarao@gmail.com>
2020-09-29 10:23:56 -0500

    Prabhakar: updated function

--- eda/eda.py
+++ eda/eda.py
@@ -1,41 +1,61 @@
 import os
 import pandas as pd
 import numpy as np
+import sys
 
-## Create a function get_data(create_csv)
-## If create_csv = True:
-##.   create a combined file rp_cons.csv from other csv files and return a dataframe rp_cons
-## If create_csv = False:
-##    return a data_frame with all columns from rp_cons.csv
-def get_data (create_csv):
+# A function named parse_2019(df) that takes a dataframe as the input.
+# It takes in a dataframe, looks for relevant columns, and then keeps the rows that
+# are in the year 2019.
 
-    if create_csv == False:    
-        rp_cons = pd.read_csv("data/rp_cons.csv")   
-    return rp_cons
+def parse_year(df, year):
+    if 'DocumentDate' in df.columns:
+        df = df[pd.to_datetime(df['DocumentDate']).dt.year == year ]
+        df.loc[:,'DocumentDate'] = str(year)
+        return df
     
-    df_rp_sales = get_sales()
-    df_parcel = get_parcels()
-    df_res_bldg  = get_resBldg()
-    df_unit_breakdown = get_unit_breakdown()
-    
-    return df_rp_sales, df_parcel, df_res_bldg, df_unit_breakdown
+# Funtion to pad zeros to Major, Minor columns, if the length is less than 
+# Major: Column 0; Minor: Column 1
+def pad_zeros_tokey(df):
+    df['Minor'] = df.Minor.astype(str).str.pad(6,fillchar='0')
+    df['Major'] = df.Major.astype(str).str.pad(6,fillchar='0')
+    return df
 
-# Data File: EXTR_RPSale.csv -------------------------------------------------------------
+#Combine Major+Minor in one column'Merged_Key' and drop Major, Minor columns
+def merge_keys (df):
+    df['Merged_Key'] = df['Major'] + df['Minor']
+    df_t = df.pop('Merged_Key')
+    df.insert(0, 'Merged_Key', df_t)
+    del df['Major'], df['Minor']
+    return df
+    
+# Data File: EXTR_RPSale.csv 
 #Table: EXTR_RPSale 
-#Keys: Major, Minor
+#Keys: Major, Minorf
 #Selected Columns ['Major', 'Minor', 'DocumentDate', 'SalePrice', 'PropertyType',
 #       'PrincipalUse', 'PropertyClass']
-
-def get_sales():
-    df_rp_sales = pd.read_csv('data/EXTR_RPSale.csv', encoding = "ISO-8859-1", low_memory=False)
-    print("Before Filer EXTR_RPSale.csv: ", df_rp_sales.shape)
+# Filters:
+#     1) Select only specified 'year' data
+#.    2) If 'SalesPrice' is 0, drop the records
+def get_sales(year):
+    df = pd.read_csv("./data/EXTR_RPSale.csv", encoding = "ISO-8859-1", low_memory=False)
 
     # Filter the following columns from EXTR_RPsale table
-    cols = list(df_rp_sales.columns)
-    df_rp_sales = df_rp_sales[cols[1:5] + cols[14:16] + cols[22:23]]
-    print("After Filer EXTR_RPSale.csv: ", df_rp_sales.shape)  
+    cols = list(df.columns)
+    df = df[cols[1:5] + cols[14:16] + cols[22:23]]
     
-    return df_rp_sales
+    #Convert mm/dd/yyyy to yyyy for further filtering
+    df = parse_year(df,year)
+    
+    #Drop zero SalePrice records
+    df = df[df['SalePrice'] != 0 ]
+    
+    #Pad zeros to keys
+    df = pad_zeros_tokey(df)
+    
+    #Combine Major+Minor in one column'Merged_Key' and drop Major, Minor columns
+    df = merge_keys(df)
+    print("Done eading Sales data....", df.shape)   
+    return df
 
 #Data File: EXTR_Parcel.csv
 #Table: EXTR_Parcel
@@ -47,18 +67,31 @@ def get_sales():
 #       'WfntRestrictedAccess', 'WfntAccessRights', 'WfntProximityInfluence',
 #       'TidelandShoreland', 'TrafficNoise', 'AirportNoise', 'PowerLines',
 #       'OtherNuisances']
-
 def get_parcels():
-    df_parcel = pd.read_csv('../data/EXTR_Parcel.csv', encoding = "ISO-8859-1", low_memory=False)
-    print("Before EXTR_Parcel.csv: ", df_parcel.shape)
-    df_parcel.columns
+    df = pd.read_csv('./data/EXTR_Parcel.csv', encoding = "ISO-8859-1", low_memory=False)
+    print("Before EXTR_Parcel.csv: ", df.shape)
+    df.columns
     
     # Filter the following columns from EXTR_Parcel table
-    cols = list(df_parcel.columns)
-    df_parcel = df_parcel[cols[:2] + cols[10:13] + cols[21:25] + cols[35:36]   + cols[37:49] + cols[50:54]]  
+    cols = list(df.columns)
+    df = df[cols[:2] + cols[10:13] + cols[15:16] + cols[21:25] + cols[35:36]   + cols[37:49] + cols[50:54]] 
     
-    print("After Filer EXTR_Parcel.csv: ", df_parcel.shape)
-    return df_parcel
+    # Filter 'KING' county only
+    
+    #Pad zeros to keys
+    df = pad_zeros_tokey(df)
+    
+    # Filter KING County only
+    df = df[df['DistrictName'].str.contains('KING', na=False)]
+    print( "After filtering KING county rows", df.shape)
+    
+    # Filter Proptype R:Residential; K:Condominium
+    df = df[(df['PropType'] == 'R') | (df['PropType'] == 'K')]
+    print("Filtering Residential and Condo data....", df.shape)   
+    
+    #Combine Major+Minor in one column'Merged_Key' and drop Major, Minor columns
+    df = merge_keys(df)
+    return df
 
 #Data File: EXTR_ResBldg.csv
 #Table: EXTR_ResBldg
@@ -73,22 +106,77 @@ def get_parcels():
 #       'FpMultiStory', 'YrRenovated', 'PcntComplete'],
 
 def get_resBldg():
-    df_res_bldg = pd.read_csv('../data/EXTR_ResBldg.csv', encoding = "ISO-8859-1", low_memory=False)
-    print("EXTR_ResBldg.csv: ", df_res_bldg.shape)
+    df = pd.read_csv('./data/EXTR_ResBldg.csv', encoding = "ISO-8859-1", low_memory=False)
 
     # Filter the following columns from EXTR_ResBldg table
-    cols = list(df_res_bldg.columns)
-    df_res_bldg = df_res_bldg[cols[:6]  + cols[11:13] + cols[15:24] + cols[26:32] + cols[35:41] +  cols[44:46]]   
-    print("After Filer EXTR_ResBldg.csv: ", df_res_bldg.shape)
+    cols = list(df.columns)
+    df = df[cols[:6]  + cols[11:13] + cols[15:24] + cols[26:32] + cols[35:41] +  cols[44:46]] 
     
-    return df_res_bldg
+    #Pad zeros to keys
+    df = pad_zeros_tokey(df)
+    
+    #Combine Major+Minor in one column'Merged_Key' and drop Major, Minor columns
+    df = merge_keys(df)
+    
+    print("After reading EXTR_ResBldg.csv: ", df.shape)    
+    return df
+
+#Data File: EXTR_LookUp.csv
+#Table: EXTR_LookUp
+#Keys: LUType, LUItem
+# Selected Columns ['LUType', 'LUItem', 'LUDescription']
+def get_lookup():
+    df = pd.read_csv('./data/EXTR_LookUp.csv', encoding = "ISO-8859-1", low_memory=False)
+
+    # Do n't filter records  
+    print("Done reading EXTR_LookUP.csv: ", df.shape)    
+    return df
+
+#Read and return a LookUp description for a given LUType and LUItem
+def get_ludescription(df, type, item):
+    return df[type][item]
 
 #Data File: EXTR_UnitBreakdown.csv
 #Table: EXTR_UnitBreakdown
 #Keys: Major, Minor
 # All columns (['Major', 'Minor', 'UnitTypeItemId', 'NbrThisType', 'SqFt', 'NbrBedrooms', 'NbrBaths'],
 def get_unitbreakdown():
-    df_unit_breakdown = pd.read_csv('../data/EXTR_UnitBreakdown.csv', encoding = "ISO-8859-1", low_memory=False)
-    print("EXTR_UnitBreakdown: ", df_unit_breakdown.shape)
+    df = pd.read_csv('./data/EXTR_UnitBreakdown.csv', encoding = "ISO-8859-1", low_memory=False)
     
-    return df_unit_breakdown
+    #Pad zeros to key
+    df = pad_zeros_tokey(df)
+    
+    #Combine Major+Minor in one column'Merged_Key' and drop Major, Minor columns
+    df = merge_keys(df)
+    
+    return df
+
+# #Function: create_consolidated
+# #Table: EXTR_UnitBreakdown
+# #Keys: Major, Minor
+# # All columns (['Major', 'Minor', 'UnitTypeItemId', 'NbrThisType', 'SqFt', 'NbrBedrooms', 'NbrBaths'],
+
+def consolidate_data(year=2019, create=False):
+    if os.path.isfile('./data/consolidated.csv') and (create==False):
+         df_merged = pd.read_csv('./data/consolidated.csv', encoding = "ISO-8859-1", low_memory=False)
+    else:           
+        # get read data from multiple csv files and create one consolidated data file after extracting year understudy and cleanup.
+        df_sales = get_sales(year)
+        df_parcels = get_parcels()
+        df_resbldg = get_resBldg()
+        df_unitbreakdown = get_unitbreakdown()
+        df_lookup = get_lookup()
+    
+        #Merge df_sales, df_parcels, df_resbldg on keys Major and Minor
+        print("Merging....")
+        df_merged = df_sales
+        df_merged = df_merged.merge(df_parcels, right_on=['Merged_Key'], left_on=['Merged_Key'], how='left')
+        df_merged = df_merged.merge(df_resbldg, right_on=['Merged_Key'], left_on=['Merged_Key'], how='left')          
+        print("After Merging files.csv: ", df_parcels.shape)   
+        print("Created merged file...s")
+        df_merged.to_csv ('./data/consolidated.csv', index = False, header=True)
+        print("Merging....Done")        
+    
+    return df_merged
+        
+
